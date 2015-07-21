7d6f2d4d85c9a2d3177f9c0d45b59e3e0549c45c
phillipgray <escapethegray11235@gmail.com>
2015-07-21 22:49:04 +0000

    adds some of the logic for storing battles

--- scoreboard.rb
+++ scoreboard.rb
@@ -5,6 +5,8 @@ require 'yaml/store'
 # to start the server:
 # "ruby basics.rb -p $PORT -o $IP"
 
+
+
 get '/' do
     @title = "Home"
     @section_title = "Current Challenges"
@@ -18,5 +20,12 @@ get '/new' do
 end
 
 post '/new' do
+    @title = "Added"
+    @section_title = "New code battle successfully added!"
+    @store = YAML::Store.new 'battles.yml'
+    @store.transaction do
+        @newest_battle = @store[:newest_battle_id]
+        @newest_battle += 1
+        @store['battle_#{@newest_battle}'] ||= {}
     
 end
